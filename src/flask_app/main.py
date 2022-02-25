da41f4c0661e5beaf75141b18d530b98b1b7ec4f
bhuiyr2 <bhuiyr2@mcmaster.ca>
2022-02-25 16:06:05 -0500

    Attempted to create the method that connects the backend sudoku recognition and solution algorithm to the frontend display.

--- src/flask_app/main.py
+++ src/flask_app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect
+from solver import solve
+from util import convertInput, generateRandomValidBoard, isBoardValid
+from sudokucv.sudokucv import SudokuCV
 
 app = Flask(__name__)
 
@@ -14,8 +17,21 @@ def upload():
 
 @app.route("/submit.html", methods = ["POST"])
 def submit():
+    # store the file in an image
     image = request.files['formFile']
-    return redirect("/")
+
+    # create a CV model that will analyze the image
+    cv = SudokuCV("sudokucv/model/handwritten_printed.h5")
+
+    # store the results of that model analysis
+    results = cv.recognize(image)
+
+    # store the board and solve it
+    bo = results.raw_results
+    bo = convertInput(bo)
+    solve(bo)
+
+    return render_template("upload.html", solvedBoard=bo)
 
 @app.route("/manual")
 def manual():
